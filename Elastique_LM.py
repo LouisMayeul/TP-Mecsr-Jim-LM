46bccc6131660dcb838b01062dc0adf6de5782c6
JimVende <jimvende72@gmail.com>
2022-01-10 15:10:43 +0100

    Update Elastique_LM.py

--- Elastique_LM.py
+++ Elastique_LM.py
@@ -28,7 +28,7 @@ def H(linéaire = True, p = 0):
         return np.array([[K/(1+e0)/p, 0], [0, 1/3*G]])
 
 def indice_des_vides(ev, e0=e0):
-    return -ev(1+e0)+e0
+    return -ev*(1+e0)+e0
 
 def Maitriser_la_contrainte(linéaire = True):
     pas_sig = np.array([10,0]) #kN
@@ -50,7 +50,7 @@ def Maitriser_la_contrainte(linéaire = True):
         PQ.append(Mp@sig)
         Epsilon_vp.append(Me@eps)
 
-    return Sigma, Epsilon, PQ, Epsilon_vp
+    return np.array(Sigma), np.array(Epsilon), np.array(PQ), np.array(Epsilon_vp)
 
 
 #pregunta para théo : el metro de los muetos, ?quitaron los asientos?
@@ -61,7 +61,7 @@ def Maitriser_contrainte_et_déplacement(linéaire = True):
     PQ      = [np.array([0,0])]
     Epsilon_vp = [np.array([0,0])]
     eps1=0
-    while Sigma[-1][0] < 500 : 
+    while Sigma[-1][0] < 50000 : 
         #calcule
         M = np.linalg.inv(Me) @ H(linéaire, PQ[-1][0]) @ Mp
         eps_1 = Epsilon[-1][0] + pas_eps
@@ -74,44 +74,65 @@ def Maitriser_contrainte_et_déplacement(linéaire = True):
         PQ.append(Mp@Sigma[-1])
         Epsilon_vp.append(Me@Epsilon[-1])
     
-    return Sigma, Epsilon, PQ, Epsilon_vp
+    return np.array(Sigma), np.array(Epsilon), np.array(PQ), np.array(Epsilon_vp)
 
 
-def print_graphe(Sigma, Epsilon, PQ, Epsilon_vp):
+def print_graphe(Sigma, Epsilon, PQ, Epsilon_vp,title):
     #TODO faire la fonction qui affiche tout bien
     
     
     figure = plt.figure(figsize = (12,9)) # pour afficher les 4 courbes en même temps pour mieux comparer les différentes méthodes
     plt.figure(1)
-    plt.suptitle("Loi de Hooke")
+    plt.suptitle(title)
     
-    plt.subplot(3,2,1)
+    plt.subplot(2, 3, 1)
     plt.plot(Sigma[:,0],Sigma[:,1],color='#9F00EC')
     plt.title('$\sigma_1$ en fonction de $\sigma_3$')
+    plt.xlabel('$\sigma_3$')
+    plt.ylabel('$\sigma_1$')
     
-    plt.subplot(3, 2, 2)
+    plt.subplot(2, 3, 2)
     plt.plot(PQ[:,0],PQ[:,1],color='green')
     plt.title("p en fonction de q")
+    plt.xlabel('q')
+    plt.ylabel('p')
     
-    plt.subplot(3, 2, 3)
+    plt.subplot(2, 3, 3)
     plt.plot(PQ[:,0],indice_des_vides(Epsilon_vp[:,0]),color='blue') # /!\ il faut remplacer epsilon par e
     plt.title("p en fonction de e")
+    plt.xlabel('e')
+    plt.ylabel('p')
     
-    plt.subplot(3, 2, 4)
-    plt.plot(np.ln(PQ[:,0]),indice_des_vides(Epsilon_vp[:,0]),color='red')
+    plt.subplot(2, 3, 4)
+    plt.plot(np.log(PQ[:,0]),indice_des_vides(Epsilon_vp[:,0]),color='red')
     plt.title("ln(p) en fonction de e")
+    plt.xlabel('e')
+    plt.ylabel('ln(p)')
     
-    plt.subplot(3, 2, 5)
+    plt.subplot(2, 3, 5)
     plt.plot(Epsilon[:,0],Epsilon_vp[:,0],color='red')
     plt.title("$\epsilon_1$ en fonction de $\epsilon_v$")
+    plt.xlabel('$\epsilon_3$')
+    plt.ylabel('$\epsilon_1$')
     
-    plt.subplot(3, 2, 6)
+    plt.subplot(2, 3, 6)
     plt.plot(Epsilon[:,0],PQ[:,1],color='green')
     plt.title("$\epsilon_1$ en fonction de q")
+    plt.xlabel('q')
+    plt.ylabel('$\epsilon_1$')
     plt.show()
 
     
     return None
 
+Sigma, Epsilon, PQ, Epsilon_vp = Maitriser_la_contrainte()
+print_graphe(Sigma, Epsilon, PQ, Epsilon_vp,"Loi Linéaire - Contrainte")
 
-print(Maitriser_contrainte_et_déplacement())
+Sigma, Epsilon, PQ, Epsilon_vp = Maitriser_la_contrainte(False)
+print_graphe(Sigma, Epsilon, PQ, Epsilon_vp,"Loi non Linéaire - Contrainte")
+
+Sigma, Epsilon, PQ, Epsilon_vp = Maitriser_contrainte_et_déplacement()
+print_graphe(Sigma, Epsilon, PQ, Epsilon_vp,"Loi Linéaire - Contrainte et Déplacement")
+
+Sigma, Epsilon, PQ, Epsilon_vp = Maitriser_contrainte_et_déplacement(False)
+print_graphe(Sigma, Epsilon, PQ, Epsilon_vp,"Loi non Linéaire - Contrainte et Déplacement")
